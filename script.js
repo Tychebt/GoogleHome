c3af5ab014621ddcbf7c715705e1310f48159842
Tychebt <masse.ju35@gmail.com>
2021-04-14 14:53:32 +0200

    Abandon du dropdown cliquable

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-<script>
 /* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
+toggle between hiding and showing the dropdown content 
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+  if (!event.target.matches('.icoApp')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
@@ -18,4 +17,3 @@ window.onclick = function(event) {
     }
   }
 }
-</script>
