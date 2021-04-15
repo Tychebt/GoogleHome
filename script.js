ebe0374a189e4abe40c0653011aaad2383baab89
Tychebt <masse.ju35@gmail.com>
2021-04-15 11:28:51 +0200

    Dropdown fini ?

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+/* Dropdown */
 function maFunction(){
   let drop = document.getElementById('drop');
 if(drop.className == 'active'){
   drop.classList.remove('active')
+  drop.classList.add('inactive')
 }
 else{
   drop.classList.add('active')
+  drop.classList.remove('inactive')
 }
 }
