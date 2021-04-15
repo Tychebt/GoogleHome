243c81b9af8359164882201e6abb57203a826ab8
Tychebt <masse.ju35@gmail.com>
2021-04-15 10:27:15 +0200

    Drop down début

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content 
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+function maFunction(){
+  let drop = document.getElementById('drop');
+if(drop.className == 'active'){
+  drop.classList.remove('active')
 }
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.icoApp')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+else{
+  drop.classList.add('active')
+}
 }
